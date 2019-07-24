72b7c9fec3ceb983722c9b35e58cd41f63b7694c
kulaev <48673312+kulaev@users.noreply.github.com>
2019-07-24 22:15:25 +0300

    Add base GUI

--- DogsTracker/Views/MainWindow.xaml.cs
+++ DogsTracker/Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DogsTracker.Views;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DogsTracker
+namespace DogsTracker.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
